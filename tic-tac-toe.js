f99a0d46b500d8316ae913cbf5c98a1dd8f618c9
tejvardhan <tejvardhan611@gmail.com>
2020-12-15 20:53:46 +0530

    adding jquery, bootstrap css and background files

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -133,7 +133,7 @@ $(document).ready(function(){
                 }
             }
             
-            console.log(state[0],state[1],state[2],state[3],state[4],state[5],state[6],state[7]);
+            
             
     
         }
